fc178f3724bbef2fddf7dbb7967110b571ded99a
Sian Alcock <sian.alcock@lawena.org>
2019-11-02 15:22:09 +0000

    Set pagination to 50 to see if the performance is the cause of the 503 errors on heroku

--- project/settings.py
+++ project/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 1000,
+    'PAGE_SIZE': 50,
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
