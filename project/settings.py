18f214dfb3b1953a257f2abc0842ba06b67be0f8
Sian Alcock <sian.alcock@lawena.org>
2021-01-01 16:38:14 +0000

    Tried commenting out clickjacking middleware to fix cors issue

--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
