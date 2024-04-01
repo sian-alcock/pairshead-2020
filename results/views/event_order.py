fea1d44233f59e046fd3b3d0ae12b3428114cf15
Sian Alcock <sian.alcock@lawena.org>
2024-04-01 14:57:59 +0100

    fix: fix csv imports

--- results/views/event_order.py
+++ results/views/event_order.py
@@ -1,174 +1,35 @@
-import os
-import csv, sys
-import tempfile
+import csv
+from .helpers import decode_utf8
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.parsers import MultiPartParser, FormParser, ParseError
+from rest_framework.parsers import MultiPartParser, FormParser
 from ..serializers import WriteEventOrderSerializer
-from ..models import EventOrder, Crew
-
-class ImportEventOrderCSVFolder(APIView):
-    # This function imports the csv from the projects folder
-    # Ideally replace with one imported via the frontend below
-    # Start by deleting all existing event cats
-
-    def get(self, _request):
-        EventOrder.objects.all().delete()
-        script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-        rel_path = "../csv/event_order.csv"
-        abs_file_path = os.path.join(script_dir, rel_path)
-
-        with open(abs_file_path, newline='') as f:
-            reader = csv.reader(f)
-            next(reader) # skips the first row
-
-            for row in reader:
-
-                if row:
-                    data = {
-                        'event': row[0],
-                        'event_order': row[1]
-                    }
-                    serializer = WriteEventOrderSerializer(data=data)
-                    if serializer.is_valid():
-                        serializer.save()
-
-            event_orders = EventOrder.objects.all()
-
-            serializer = WriteEventOrderSerializer(event_orders, many=True)
-
-            self.calculate_computed_properties()
-
-
-            return Response(serializer.data)
-        
-    def calculate_computed_properties(self):
-
-        for crew in Crew.objects.all():
-            crew.event_band = crew.calc_event_band()
-            crew.draw_start_score = crew.calc_draw_start_score()
-            crew.calculated_start_order = crew.calc_calculated_start_order()
-            crew.save()
-
-# class EventOrderImport(APIView):
-#     # This function ATTEMPTS to import the csv from frontend
-#     # Should replace the function above when working
-#     # Start by deleting all existing event cats
-
-#     parser_classes = (FormParser, MultiPartParser)
-
-#     def post(self, request):
-#         EventOrder.objects.all().delete()
-#         # Convert the InMemoryUploadedFile to a NamedTemporaryFile
-#         for csv_upload in request.FILES.values():
-#             file_temp = tempfile.NamedTemporaryFile()
-#             file_temp.write(csv_upload.read())
-#             print(file_temp.name) # This is the path.
-
-#             with open(file_temp.name, newline='') as f:
-#                 reader = csv.reader(f)
-#                 next(reader) # skips the first row
-
-#                 for row in reader:
-
-#                     if row:
-#                         data = {
-#                             'event': row[0],
-#                             'event_order': row[1]
-#                         }
-#                         serializer = ImportEventOrderSerializer(data=data)
-#                         if serializer.is_valid():
-#                             serializer.save()
-
-#                 event_orders = EventOrder.objects.all()
-
-#                 serializer = ImportEventOrderSerializer(event_orders, many=True)
-
-#                 self.calculate_computed_properties()
-
-#                 file_temp.close()
-
-#                 return Response(serializer.data)
-
-#         return Response({"Success!": "CSV imported OK"})
-
-#     def calculate_computed_properties(self):
-
-#         for crew in Crew.objects.all():
-#             crew.event_band = crew.calc_event_band()
-#             crew.save()
-
-
-
-# Attempt to read in the event order CSV from the front end
+from ..models import EventOrder
 
 class EventOrderImport(APIView):
+    # This function ATTEMPTS to import the csv from frontend
+    # Start by deleting all existing event cats
+
     parser_classes = (FormParser, MultiPartParser)
 
     def post(self, request):
         EventOrder.objects.all().delete()
-        # Convert the InMemoryUploadedFile to a NamedTemporaryFile
-        for csv_upload in request.FILES.values():
-            file_temp = tempfile.NamedTemporaryFile()
-            file_temp.write(csv_upload.read())
-            print(file_temp.name) # This is the path.
 
-            with open(file_temp.name, newline='', encoding='utf-8') as f:
-                reader = csv.reader(f)
-                # print(reader)
-                print('we have a reader if we are here')
-                next(reader) # skips the first row
+        reader = csv.reader(decode_utf8(request.FILES['file']))
+        next(reader) # skips the first row
+        for row in reader:
 
-                # check_file = os.path.getsize(file_temp.name)
+            if row:
+                data = {
+                    'event': row[0],
+                    'event_order': row[1]
+                }
+                serializer = WriteEventOrderSerializer(data=data)
+                if serializer.is_valid():
+                    serializer.save()
 
-                # if(check_file == 0):
-                #     print("The file is empty.")
-                #     return Response({"Bummer!": "There is no data or data is not recognized"})
-                # else:
-                #     print("The file is not empty.")
+        event_orders = EventOrder.objects.all()
 
-                #     result = []
+        serializer = WriteEventOrderSerializer(event_orders, many=True)
 
-                #     for row in reader:
-                #         print('are we getting to here within the row??')
-                #         result.append(row[0])
-
-                #     print(result)
-
-                try:
-                    for row in reader:
-                        if row:
-                            data = {
-                                'event': row[0],
-                                'event_order': row[1]
-                            }
-                            serializer = WriteEventOrderSerializer(data=data)
-                            if serializer.is_valid(raise_exception=True):
-                                serializer.save()
-
-                            event_orders = EventOrder.objects.all()
-
-                            serializer = WriteEventOrderSerializer(event_orders, many=True)
-
-                            # self.calculate_computed_properties()
-
-                            file_temp.close()
-
-                            return Response(serializer.data)
-                
-                except csv.Error as e:
-                    sys.exit('file {}, line {}: {}'.format(file_temp.name, reader.line_num, e))
-
-
-
-        return Response({"Success!": "CSV imported OK"})
-    
-    # def calculate_computed_properties(self):
-
-    #     for crew in Crew.objects.all():
-    #         crew.event_band = crew.calc_event_band()
-    #         crew.draw_start_score = crew.calc_draw_start_score()
-    #         crew.calculated_start_order = crew.calc_calculated_start_order()
-    #         crew.save()
-
-
+        return Response(serializer.data)
--- results/views/original_event_category.py
+++ results/views/original_event_category.py
@@ -1,11 +1,10 @@
 import csv
-import os
-import tempfile
+from .helpers import decode_utf8
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.parsers import MultiPartParser, FormParser, ParseError
+from rest_framework.parsers import MultiPartParser, FormParser
 from ..serializers import ImportOriginalEventSerializer
-from ..models import OriginalEventCategory, Crew
+from ..models import OriginalEventCategory
 
 
 class OriginalEventCategoryImport(APIView):
@@ -13,66 +12,23 @@ class OriginalEventCategoryImport(APIView):
 
     def post(self, request):
         OriginalEventCategory.objects.all().delete()
-        # Convert the InMemoryUploadedFile to a NamedTemporaryFile
-        for csv_upload in request.FILES.values():
-            file_temp = tempfile.NamedTemporaryFile()
-            file_temp.write(csv_upload.read())
-            print(file_temp.name) # This is the path.
 
-            with open(file_temp.name, newline='') as f:
-                reader = csv.reader(f)
-                next(reader) # skips the first row
+        reader = csv.reader(decode_utf8(request.FILES['file']))
+        next(reader) # skips the first row
+        for row in reader:
 
-                for row in reader:
+            if row:
+                data = {
+                    'crew': row[0],
+                    'event_original': row[1]
+                }
+                serializer = ImportOriginalEventSerializer(data=data)
+                if serializer.is_valid():
+                    serializer.save()
 
-                    if row:
-                        data = {
-                            'crew': row[0],
-                            'event_original': row[1]
-                        }
-                        serializer = ImportOriginalEventSerializer(data=data)
-                        if serializer.is_valid(raise_exception=True):
-                            serializer.save()
+        event_categories = OriginalEventCategory.objects.all()
 
-                original_event_categories = OriginalEventCategory.objects.all()
+        serializer = ImportOriginalEventSerializer(event_categories, many=True)
 
-                serializer = ImportOriginalEventSerializer(original_event_categories, many=True)
+        return Response(serializer.data)
 
-                file_temp.close()
-
-                return Response(serializer.data)
-
-        return Response({"Success!": "CSV imported OK"})
-
-
-class ImportOriginalEventCategoryCSVFolder(APIView):
-    # This function imports the csv from the projects folder
-    # Ideally replace with one imported via the frontend (from others that are working)
-    # Start by deleting all existing race times
-
-    def post(self, _request):
-        OriginalEventCategory.objects.all().delete()
-        script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-        rel_path = "../csv/original_event_categories.csv"
-        abs_file_path = os.path.join(script_dir, rel_path)
-
-        with open(abs_file_path, newline='') as f:
-            reader = csv.reader(f)
-            next(reader) # skips the first row
-            
-            for row in reader:
-
-                if row:
-                    data = {
-                        'crew': row[0],
-                        'event_original': row[1]
-                    }
-                    serializer = ImportOriginalEventSerializer(data=data)
-                    if serializer.is_valid(raise_exception=True):
-                        serializer.save()
-
-            original_event_categories = OriginalEventCategory.objects.all()
-
-            serializer = ImportOriginalEventSerializer(original_event_categories, many=True)
-
-            return Response(serializer.data)
