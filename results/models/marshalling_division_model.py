27bb1b3f4de33647b8b97479c08110b10562384e
Sian Alcock <sian.alcock@lawena.org>
2024-04-01 15:22:51 +0100

    fix: remove id fields

--- results/models/marshalling_division_model.py
+++ results/models/marshalling_division_model.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class MarshallingDivision(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     bottom_range=models.IntegerField()
     top_range=models.IntegerField()
--- results/models/number_location_model.py
+++ results/models/number_location_model.py
@@ -1,6 +1,5 @@
 from django.db import models
 
 class NumberLocation(models.Model):
-    id = models.IntegerField(primary_key=True)
     club = models.CharField(max_length=50)
     number_location=models.CharField(max_length=50)
