f9c0f156b658a322cd377f290c6d1ffbc8ca99d6
Sian Alcock <sian.alcock@lawena.org>
2024-08-04 18:04:13 +0100

    feat: upgrades

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'production',
   entry: './src/app.js',
   context: path.resolve(__dirname, 'frontend'),
   output: {
@@ -10,26 +11,42 @@ module.exports = {
     path: path.resolve(__dirname, 'frontend/dist'),
     publicPath: '/'
   },
-  devtool: 'source-maps',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    fallback: { 
+      'buffer': require.resolve('buffer/'),
+      'stream': require.resolve('stream-browserify'),
+      'crypto': require.resolve('crypto-browserify'),
+      'vm': require.resolve('vm-browserify'),
+      'util': require.resolve('util')
+    }
+  },
+  devtool: 'source-map',
   module: {
     rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.woff2?$/, loader: 'file-loader' },
-      { test: /\.(jpg|png|gif)$/, loader: 'file-loader' }
+      {
+        test: /.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.s(a|c)ss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.woff2?$/, use: 'file-loader' },
+      { test: /\.(jpg|png|gif)$/, use: 'file-loader' }
     ]
   },
   devServer: {
-    contentBase: 'src',
-    hot: true,
+    static: 'src',
     open: true,
     port: 8000,
-    watchContentBase: true,
     historyApiFallback: true,
-    proxy: {
-      '/api': 'http://localhost:4000'
-    }
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:4000'
+      }
+    ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -37,6 +54,12 @@ module.exports = {
       template: 'src/index.html',
       filename: 'index.html',
       inject: 'body'
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer']
     })
   ]
 }
