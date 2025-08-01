6af87687130b3bc0eb4d1631a48eb5454cd85937
Sian Alcock <sian.alcock@lawena.org>
2025-08-01 08:29:53 +0100

    new icons

--- frontend/src/components/atoms/Icons/Icons.tsx
+++ frontend/src/components/atoms/Icons/Icons.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement } from "react";
 
 export type IconType =
+  | 'save'
+  | 'edit'
   | 'add'
   | 'sort'
   | 'sort-asc'
@@ -38,6 +40,32 @@ export default function Icon({
   ariaHidden = false
 }: IconProps): ReactElement {
   switch (icon) {
+    case "save":
+      return (
+        <svg 
+          className="icon__save"
+          aria-hidden={ariaHidden}
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="currentColor">
+            <path d="M840-680v480q0 33-23.5 56.5T760-120H200q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h480l160 160Zm-80 34L646-760H200v560h560v-446ZM480-240q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM240-560h360v-160H240v160Zm-40-86v446-560 114Z"/>
+        </svg>
+      )
+    case "edit":
+      return (
+        <svg 
+          className="icon__edit"
+          aria-hidden={ariaHidden}
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="currentColor">
+            <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/>
+        </svg>
+      )
     case "add":
       return (
         <svg
