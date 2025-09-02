d46fc46dc15f969548f13380cc26b04977049768
Sian Alcock <sian.alcock@lawena.org>
2025-09-02 13:26:20 +0100

    fix reinstate setting that points to postgres

--- project/settings.py
+++ project/settings.py
@@ -153,4 +153,4 @@ REST_FRAMEWORK = {
 }
 
 django_heroku.settings(locals())
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
