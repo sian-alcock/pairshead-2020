5b650d7b4de3daf6ad5859aebbab9165e9b71cb1
Sian Alcock <sian.alcock@lawena.org>
2025-09-02 18:17:20 +0100

    add requirements and undo debug

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-# urlpatterns = [
-#     path('', TemplateView.as_view(template_name='index.html'), name='home'),
-# ]
-from .views import health_check
 urlpatterns = [
-    path('', health_check),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
--- frontend/views.py
+++
@@ -1,3 +0,0 @@
-from django.http import HttpResponse
-def health_check(request):
-    return HttpResponse("Hello World!")
