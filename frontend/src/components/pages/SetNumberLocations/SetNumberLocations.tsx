38feddd3a72e129140e3caa2dd195e976dae3dc7
Sian Alcock <sian.alcock@lawena.org>
2025-09-17 22:23:54 +0100

    fix: remove comment out numberlocations table

--- frontend/src/components/pages/SetNumberLocations/SetNumberLocations.tsx
+++ frontend/src/components/pages/SetNumberLocations/SetNumberLocations.tsx
@@ -2,20 +2,26 @@ import React from "react";
 import Hero from "../../organisms/Hero/Hero";
 import Header from "../../organisms/Header/Header";
 import "./setNumberLocations.scss";
-import NumberLocationsManager from "../../organisms/NumberLocationsManager/NumberLocationsManager";
+// import NumberLocationsManager from "../../organisms/NumberLocationsManager/NumberLocationsManager";
+import DataExportComponent from "../../molecules/DataExportComponent/DataExportComponent";
+import { CSVUploadModal } from "../../molecules/CSVUploadModal/CSVUploadModal";
 
 export default function SetNumberLocations() {
-  const exportNumberLocationTemplate = (e: { preventDefault: any }) => {
-    e.preventDefault;
-    window.open("api/number-location-template/");
-  };
   return (
     <>
       <Header />
       <Hero title={"Set Number locations for host clubs"} />
       <section className="set-number-location__section">
         <div className="set-number-location__container">
-          <NumberLocationsManager />
+          {/* <NumberLocationsManager /> */}
+          <div className="set-number-location__import-wrapper">
+            <div className="set-number-location__button">
+              <DataExportComponent url={"/api/number-location-template/"} buttonText={"Export template"} />
+            </div>
+            <div className="set-number-location__button">
+              <CSVUploadModal url={"api/number-location-import/"} />
+            </div>
+          </div>
         </div>
       </section>
     </>
