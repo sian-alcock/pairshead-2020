2dc1dfed8494c66f599e5a75ea503933141069e9
Sian Alcock <sian.alcock@lawena.org>
2025-09-26 15:26:41 +0100

    comment out dev tools

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -89,7 +89,7 @@ function App() {
         </HashRouter>
       </div>
       {/* Add React Query DevTools in development */}
-      {process.env.NODE_ENV === "development" && <ReactQueryDevtools initialIsOpen={false} />}
+      {/* {process.env.NODE_ENV === "development" && <ReactQueryDevtools initialIsOpen={false} />} */}
     </QueryClientProvider>
   );
 }
